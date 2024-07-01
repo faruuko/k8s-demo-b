67c082a65438d13cc0bf78fa19783f7310484701
Faruq Alli-Balogun <ab.faruq@sten.agency>
2024-07-01 14:39:18 +0100

    ...

--- app.ts
+++ app.ts
@@ -1,22 +1,36 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
 
-app.get("/route-a", async (_req, res) => {
+const requiresAuth = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.headers["x-user-id"];
+
+    if (user) {
+      next();
+    } else {
+      res.status(403).json({
+        message: "A valid access token is required...",
+      });
+    }
+  };
+};
+
+app.get("/route-a", requiresAuth(), async (_req, res) => {
   res.status(200).json({
     message: "k8s-demo-b",
     path: "route-a",
   });
 });
 
-app.get("/route-b", async (req, res) => {
+app.get("/route-b", requiresAuth(), async (_req, res) => {
   res.status(200).json({
     message: "k8s-demo-b",
     path: "route-b",
   });
 });
 
-app.get("/route-c", async (req, res) => {
+app.get("/route-c", async (_req, res) => {
   res.status(200).json({
     message: "k8s-demo-b",
     path: "route-c",
